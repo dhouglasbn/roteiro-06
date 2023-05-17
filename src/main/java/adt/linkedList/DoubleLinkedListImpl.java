a9432d49804f00e8bf904a5ce6c43663456a7778
dhouglas.nobrega <dhouglas.nobrega@lcc3-044>
2023-05-17 09:59:08 -0300

    fix: fixing casting problem

--- src/main/java/adt/linkedList/DoubleLinkedListImpl.java
+++ src/main/java/adt/linkedList/DoubleLinkedListImpl.java
@@ -4,24 +4,98 @@ public class DoubleLinkedListImpl<T> extends SingleLinkedListImpl<T> implements
 		DoubleLinkedList<T> {
 
 	protected DoubleLinkedListNode<T> last;
+	
+	public DoubleLinkedListImpl() {
+		super();
+		this.head = new DoubleLinkedListNode<T>();
+		
+		this.last = (DoubleLinkedListNode<T>)this.head;
+	}
+	
+	@Override
+	public int size() {
+		int size = 0;
+		DoubleLinkedListNode<T> node = (DoubleLinkedListNode<T>) this.head;
+		while (!node.isNIL()) {
+			size++;
+			node = (DoubleLinkedListNode<T>) node.getNext();
+		}
+		return size;
+	}
+
+	@Override
+	public T search(T element) {
+		DoubleLinkedListNode<T> auxHead = (DoubleLinkedListNode<T>) this.getHead();
+		while (!auxHead.isNIL() && auxHead.getData() != element) {
+			auxHead = (DoubleLinkedListNode<T>) auxHead.getNext();
+		}
+		return auxHead.getData();
+	}
+
+	@Override
+	public void insert(T element) {
+		if (element != null) {
+			DoubleLinkedListNode<T> auxHead = (DoubleLinkedListNode<T>) this.head;
+			if (this.head.isNIL()) {
+				DoubleLinkedListNode<T> nil = new DoubleLinkedListNode<T>();
+				DoubleLinkedListNode<T> newHead = new DoubleLinkedListNode<T>(element, nil, nil);
+				this.head = newHead;
+			} else {
+				while(!auxHead.getNext().isNIL()) {
+					auxHead = (DoubleLinkedListNode<T>) auxHead.getNext();
+				}
+				
+				DoubleLinkedListNode<T> nil = new DoubleLinkedListNode<T>();
+				auxHead.setNext(new DoubleLinkedListNode<T>(element, auxHead, nil));
+			}			
+		}
+	}
+
+	@Override
+	public void remove(T element) {
+		if (element != null) {
+			if (head.getData() == element) {
+				this.head = this.head.getNext();
+			} else {
+				DoubleLinkedListNode<T> aux = (DoubleLinkedListNode<T>) this.head;
+				while (!aux.isNIL() && aux.getData() != element) {
+					aux = (DoubleLinkedListNode<T>) aux.getNext();
+				}
+				
+				if (!aux.isNIL()) {					
+					DoubleLinkedListNode<T> next = (DoubleLinkedListNode<T>) aux.getNext();
+					aux.data = aux.next.getData();
+					aux.next = next;
+					next.previous = aux.previous;
+				}
+			}
+		}
+	}
+
+	@Override
+	public T[] toArray() {
+		T[] result = (T[]) new Object[this.size()];
+		DoubleLinkedListNode<T> aux = (DoubleLinkedListNode<T>) this.head;
+		
+		for (int i = 0; i < this.size(); i++) {
+			result[i] = aux.getData();
+			aux = (DoubleLinkedListNode<T>) aux.getNext();			
+		}
+		
+		return result;
+	}
 
 	@Override
 	public void insertFirst(T element) {
 		DoubleLinkedListNode<T> nil = new DoubleLinkedListNode<T>();
-		DoubleLinkedListNode<T> auxHead = this.last;
+		DoubleLinkedListNode<T> auxHead = (DoubleLinkedListNode<T>) this.head;
+		DoubleLinkedListNode<T> newHead = new DoubleLinkedListNode<T>(element, auxHead, nil);
+		auxHead.previous = newHead;
 		
-		if (auxHead == null) {
-			DoubleLinkedListNode<T> newHead = new DoubleLinkedListNode<T>(element, nil, nil);
+		if (this.head.isNIL()) {
 			this.last = newHead;
-			this.head = newHead;
-		} else {
-			while(!auxHead.previous.isNIL()) {
-				auxHead = auxHead.previous;
-			}
-			
-			DoubleLinkedListNode<T> newHead = new DoubleLinkedListNode<T>(element, auxHead, nil);
-			auxHead.previous = newHead;			
 		}
+		auxHead = newHead;
 	}
 
 	@Override
@@ -30,12 +104,9 @@ public class DoubleLinkedListImpl<T> extends SingleLinkedListImpl<T> implements
 			this.head = this.head.next;
 			
 			if(this.head.isNIL()) {
-				this.last = new DoubleLinkedListNode<T>();;
+				this.last = new DoubleLinkedListNode<T>();
 			} else {
-				DoubleLinkedListNode<T> aux = this.last;
-				while(!aux.previous.previous.isNIL()) {
-					aux = aux.previous;
-				}
+				DoubleLinkedListNode<T> aux = (DoubleLinkedListNode<T>) this.head;
 				aux.previous = new DoubleLinkedListNode<T>();
 			}
 		}
@@ -47,8 +118,9 @@ public class DoubleLinkedListImpl<T> extends SingleLinkedListImpl<T> implements
 			this.last = this.last.previous;
 			
 			if(this.last.isNIL()) {
-				this.head = new SingleLinkedListNode<T>();
+				this.head = new DoubleLinkedListNode<T>();
 			} else {
+				
 				this.last.next = new DoubleLinkedListNode<T>();
 			}
 		}
